828f62673e0bb9ef4950dd99011d75d3dd935366
Gino Luiggi Chirinos Plasencia <luiggiplasencia0823@gmail.com>
2022-09-19 20:03:30 +0200

    Volviendo a su estado funcional

--- app/Repositories/ProductImage/ProductImageRepository.php
+++ app/Repositories/ProductImage/ProductImageRepository.php
@@ -20,9 +20,5 @@ class ProductImageRepository extends BaseRepository implements ProductImageRepos
     public function store(array $params): ProductImage
     {
         return $this->model->query()->create($params);
-
-
-        // TODO: pruebas
-        // TODO: otra prueba
     }
 }
