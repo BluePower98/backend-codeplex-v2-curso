5e127866181c92c49445064d1ad1358a26935a05
Edder Ivan <e_jamanca@codeplex.pe>
2022-09-24 11:13:42 -0500

    descarga de xml y cdr

--- app/Http/Controllers/Api/V1/Integrador/Comprobante/comprobanteController.php
+++ app/Http/Controllers/Api/V1/Integrador/Comprobante/comprobanteController.php
@@ -41,14 +41,23 @@ class comprobanteController extends ApiController
 
         return $this->successResponse($result);
     }
-    public function downloadXML(ComprobanteRequest $request): JsonResponse
+       /**
+     * Descargar xml de facturación electrónica.
+     *
+     * @param Request $request
+     * @throws BadRequestException
+     */
+    public function downloadXML(ComprobanteRequest $request)
     {
-        $result=$this->comprobanteservice
-        ->getNameByRuc($request->get('ruc'),
-        $request->get('date'),$request->get('filename'));
+        // dd($request->all());
+        $result = $this->comprobanteservice
+        ->getNameByRuc($request->all());
 
-        return $this->successResponse($result);
+        return response()->file($result['file'], [
+            'Content-Disposition' => 'attachment;filename="'. $result['filename'] .'"'
+        ]);
     }
+
 
     public function getDatoForExcel(Request $request): JsonResponse
     {
--- app/Services/Application/Comprobante/comprobanteService.php
+++ app/Services/Application/Comprobante/comprobanteService.php
@@ -7,6 +7,7 @@ use Illuminate\Support\Collection;
 use Exception;
 use Symfony\Component\HttpFoundation\Response;
 use Illuminate\Http\Request;
+use App\Exceptions\Owner\BadRequestException;
 
 class comprobanteService
 {
@@ -35,10 +36,24 @@ class comprobanteService
         return $this->repository->getVentaPagos($idempresa,$idtipodocumento,$serie,$numero);
     }
 
-    public function getNameByRuc(string $ruc,string $date,string $filename): Collection
+ /**
+     * Descarga de facturas con POST.
+     *
+     * @param array $params
+     * @return array
+     * @throws BadRequestException
+     */
+    public function getNameByRuc(array $params): array
     {
         // dd($ruc,$date,$filename);
-        $company=$this->repository->getNameByRuc($ruc);
+        
+        $ruc = $params['ruc'];
+        $date = $params['date'];
+        $filename = $params['filename'];
+
+        // throw new BadRequestException('El fichero no se encuentra disponible o no existe.');
+
+        $company=$this->repository->getNameByRuc($params['ruc']);
         // dd($company);
         if(!$company) {
             throw new Exception(
@@ -46,31 +61,32 @@ class comprobanteService
                 Response::HTTP_NOT_FOUND
             );
         };
+        // dd( $company);
 
         $companyNameAndRuc = substr(strrchr($company->RutFilXmlFacEleSerLoc, "\\"), 1);
         $stringDate = str_replace('-', '', $date);
-      
-
-        $server = config('services.facturacion_electronica.server');
-        $remoteFile = $server['documents']['path_xml'] . '/' . "{$companyNameAndRuc}/{$stringDate}/{$filename}";
+        // dd($companyNameAndRuc);
         
+        $server=config('services.facturacion_electronica.server');
+        // dd($server);
+        $remoteFile = $server['documents']['path_xml'] . '/' . "{$companyNameAndRuc}/{$stringDate}/{$filename}";
+        // dd($remoteFile);
         $host = $server['host'];
         $username = $server['username'];
         $password = $server['password'];
-        
         error_reporting(E_ERROR | E_PARSE | E_NOTICE);
         
         $connection = ftp_connect($host);
         // dd('ssdsd',$connection);
 
         if (!$connection) {
-            throw new Exception('Problemas de conexión con el FTP.', Response::HTTP_BAD_REQUEST);
+            throw new BadRequestException('Problemas de conexión.');
         }
 
         ftp_set_option($connection, FTP_TIMEOUT_SEC, 180);
 
         if (!ftp_login($connection, $username, $password)) {
-            throw new Exception('Problemas de conexión con el FTP.', Response::HTTP_BAD_REQUEST);
+            throw new BadRequestException('Problemas de conexión.');
         }
 
         // Turn passive mode on
@@ -81,22 +97,19 @@ class comprobanteService
        
 
         if(!$foundedFile = current($list)) {
-            throw new Exception('El fichero no existe en el FTP.', Response::HTTP_NOT_FOUND);
+            throw new BadRequestException('El fichero no existe en el FTP.');
         }
 
         $foundedFileName = pathinfo($foundedFile, PATHINFO_BASENAME);
         $foundedFileExtension = strtolower(pathinfo($foundedFile, PATHINFO_EXTENSION));
 
         if($foundedFileExtension !== 'xml'){
-            throw new Exception(
-                'Sólo se pueden descargar ficheros con extensión "xml".',
-                Response::HTTP_BAD_REQUEST
-            );
+            throw new BadRequestException('Sólo se pueden descargar ficheros con extensión "xml".');
         }
 
         // Creamos el fichero temporal
         $file = tempnam(sys_get_temp_dir(), 'tmp');
-
+        // dd($file);
         if(file_exists($file)){
             unlink($file);
         }
@@ -110,28 +123,18 @@ class comprobanteService
         if($download !== FTP_FINISHED){
             ftp_close($connection);
 
-            throw new Exception(
-                'Ocurrió un problema durante la descarga del fichero desde el FTP.',
-                Response::HTTP_BAD_REQUEST
-            );
+           
+            throw new BadRequestException('Ocurrió un problema durante la descarga del fichero desde el FTP.');
         }
 
         ftp_close($connection);
 
-        header('Content-type: "text/xml"; charset="utf8"');
-        header("Content-Disposition: attachment;filename={$foundedFileName}");
-
-        $content = file_get_contents($file);
-
-        unlink($file);
-        dd($content);
-        return collect($content);
-
-        echo $content;
-        exit();
+        return [
+            'file' => $file,
+            'filename' => $foundedFileName,
+        ];
 
     }
-
     public function getDatoForExcel(Request $request):Collection
     {
         return $this->repository->getDatoForExcel($request->all());
